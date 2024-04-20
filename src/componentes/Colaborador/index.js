782030f0339fc2cc8a99fc4b6299fa08afc35570
PPPgata <pedrodaniel8416@gmail.com>
2024-04-20 14:47:22 -0300

    organo 2.0

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,15 +1,32 @@
 import './colaborador.css'
+import { IoIosCloseCircle } from "react-icons/io";
+import { FaHeart,FaRegHeart } from "react-icons/fa";
 
-const Colaborador = ({ colaborador, corDeFundo }) => {
-    return (<div className="colaborador">
-        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
-            <img src={colaborador.imagem} alt={colaborador.nome} />
-        </div>
-        <div className="rodape">
-            <h4>{colaborador.nome}</h4>
-            <h5>{colaborador.cargo}</h5>
-        </div>
-    </div>)
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar,aoFavoritar }) => {
+    function favoritar(){
+        aoFavoritar(colaborador.id);
+    }
+    return (
+        <div className="colaborador">
+            <IoIosCloseCircle 
+                size={25} 
+                className='deletar' 
+                onClick={() => aoDeletar(colaborador.id)}
+            />
+            <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+                <img src={colaborador.imagem} alt={colaborador.nome} />
+            </div>
+            <div className="rodape">
+                <h4>{colaborador.nome}</h4>
+                <h5>{colaborador.cargo}</h5>
+                <div className='favoritar'>
+                    {colaborador.favorito 
+                        ? <FaHeart size={25} color='#ff0000' onClick={favoritar}/> 
+                        : <FaRegHeart size={25} onClick={favoritar}/>
+                    }
+                </div>
+            </div>
+        </div>)
 }
 
 export default Colaborador
